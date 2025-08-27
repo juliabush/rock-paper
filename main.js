71790b4299d7103ec798ea93755d2d6eb132ed8f
Julia Bush <j.elizabethbush@gmail.com>
2025-08-27 10:59:51 +0200

    Completing the get computer choice function that produces either rock paper or scissors

---
+++ main.js
@@ -0,0 +1,18 @@
+let computerScore = 0;
+let humanScore = 0;
+
+function getComputerChoice() {
+    let randomNumber = Math.floor(Math.random() * 3);
+    if (randomNumber === 2) {
+        return "Rock";
+    } else if (randomNumber === 1) {
+        return "Paper";
+    } else {
+        return "Scissors";
+    }
+}
+console.log(getComputerChoice());
+
+function getHumanChoice() {
+    
+}
